986b61c20ec4fb5d770bceea9d33f5389fbf5cad
ajay1706 <ajay97456@gmail.com>
2019-11-16 03:44:51 +0530

    adding the next container

--- provider_app/lib/screens/task_screen.dart
+++ provider_app/lib/screens/task_screen.dart
@@ -5,29 +5,50 @@ class TaskScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
-      body: Container(
-        padding: EdgeInsets.only(top: 60,left: 30,right: 30,bottom: 30),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(top: 60,left: 30,right: 30,bottom: 30),
 
-        child: Column(
-          children: <Widget>[
-            CircleAvatar(
-                backgroundColor: Colors.white,
-                radius: 30,
-                child: Icon(
-                  Icons.list,
-                  size: 30,
-                color: Colors.lightBlueAccent,)),
-            SizedBox(
-              height: 10,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                CircleAvatar(
+                    backgroundColor: Colors.white,
+                    radius: 30,
+                    child: Icon(
+                      Icons.list,
+                      size: 30,
+                    color: Colors.lightBlueAccent,)),
+                SizedBox(
+                  height: 10,
+                ),
+                Text('hurly-burly',
+                style: TextStyle(
+                  fontSize: 50,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.white
+                ),),
+                Text('12 Tasks',
+                  style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white
+                  ),),
+
+              ],
             ),
-            Text('hurly-burly',
-            style: TextStyle(
-              fontSize: 50,
-              fontWeight: FontWeight.w700,
-              color: Colors.white
-            ),)
-          ],
-        ),
+          ),
+          Expanded(
+            child: Container(
+
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(20),topRight: Radius.circular(20))
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
