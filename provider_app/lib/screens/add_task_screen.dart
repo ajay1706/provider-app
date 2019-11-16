a4457a73990e2465928535f4ee8d89d041b4a451
ajay1706 <ajay97456@gmail.com>
2019-11-16 15:56:51 +0530

    modalsheet done

--- provider_app/lib/screens/add_task_screen.dart
+++ provider_app/lib/screens/add_task_screen.dart
@@ -6,12 +6,40 @@ class AddTaskScreen extends StatelessWidget {
     return Container(
       color: Color(0xff757575),
       child: Container(
+        padding: EdgeInsets.all(20),
         decoration: BoxDecoration(
           color: Colors.white,
           borderRadius: BorderRadius.only(
             topRight: Radius.circular(20),
               topLeft: Radius.circular(20)
           )
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+         
+          children: <Widget>[
+            Text('Add Task',
+            style: TextStyle(
+              fontSize: 30,
+              color: Colors.lightBlueAccent,
+              fontWeight: FontWeight.w700
+            ),
+            textAlign: TextAlign.center,),
+            SizedBox(height: 20,),
+            TextField(
+              autofocus: true,
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 20,),
+            FlatButton(
+                onPressed: (){},
+                child: Text("Add",
+                style: TextStyle(
+                  color: Colors.white
+                ),),
+              color: Colors.lightBlueAccent,
+            )
+          ],
         ),
       )
     );
